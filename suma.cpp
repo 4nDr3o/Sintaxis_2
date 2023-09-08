f060aaab10ff18e691b7e22c670e99cce603f070
4nDr3o <l21140730@queretaro.tecnm.mx>
2023-09-08 13:34:14 -0600

    Updated

---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include<iostream>
+#include<conio.h>
+
+float a,b,c,d;
+
+void main() // Funcion principal
+{
+    a=(3+5)*8-(10-4)/2;
+    b=8;
+    b+=9;
+    printf("Ho\nla");
+    
+}
